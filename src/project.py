d27c85ff6db2ecb5f5e752b40f693e6359cc12e0
aaa240015 <aaa240015@utdallas.edu>
2025-04-12 08:32:06 -0500

    Initialize pygame project with main event loop

--- src/project.py
+++ src/project.py
@@ -1,1 +1,16 @@
-#Achai Arop 
+import pygame
+import sys
+pygame.init()
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+    
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
